b4fd9f84887d11aa7228c3af8967969f95aae798
PalakKapila72 <24BCS10349@cuchd.in>
2026-02-24 11:36:00 +0530

    Create Asset class with validation methods

---
+++ 21feb_2026/Asset.java
@@ -0,0 +1,26 @@
+public class Asset {
+
+    String assetId;
+    String assetName;
+    boolean available;
+    int securityLevel;
+
+    public Asset(String assetId, String assetName, boolean available, int securityLevel) {
+        this.assetId = assetId;
+        this.assetName = assetName;
+        this.available = available;
+        this.securityLevel = securityLevel;
+    }
+
+    public void validatePolicy(String uid) 
+            throws IllegalStateException, SecurityException {
+
+        if (!available) {
+            throw new IllegalStateException("Asset not available");
+        }
+
+        if (securityLevel == 3 && !uid.startsWith("KRG")) {
+            throw new SecurityException("Restricted asset. KRG UID required.");
+        }
+    }
+}
